a38ec874d09949ce1d69066a4123ccd907845959
krittikasedhain <krittikasedhain1112@gmail.com>
2022-09-17 15:18:06 -0500

    single kino drawing component created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import "./App.css";
+import KinoSingleComp from "./Components/KinoSingle.comp";
 import { useDispatch, useSelector } from "react-redux";
 import fetch_data from "./Store/actions/fetch_data.action";
 import styled from "styled-components";
@@ -18,6 +19,17 @@ function App() {
   return (
     <Container>
       <h1>Kino</h1>
+       <KinoContainer>
+        {kino.map((kinoGame, index) => (
+          <KinoSingleComp key={index} {...kinoGame} />
+        ))}
+        {kino.map((kinoGame, index) => (
+          <KinoSingleComp key={index} {...kinoGame}/>
+        ))}
+        {kino.map((kinoGame, index) => (
+          <KinoSingleComp key={index} {...kinoGame} />
+        ))}
+      </KinoContainer>
     </Container>
   );
 }
@@ -26,6 +38,25 @@ const Container = styled.div`
   background-color: black;
   height: 100vh;
   color: #fff;
+`;
+
+const KinoContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  gap: 0.5rem;
+  margin: 0 1rem;
+  @media (max-width: 1500px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+  @media (max-width: 1250px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+  @media (max-width: 950px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+  @media (max-width: 650px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
 `;
 
 export default App;
---
+++ src/Components/KinoSingle.comp.js
@@ -0,0 +1,80 @@
+import React from "react";
+import styled from "styled-components";
+
+const KinoSingleComp = ({
+  gameNumber,
+  gameDate,
+  bonus,
+  drawNumbers,
+  click,
+}) => {
+  return (
+    <Container
+      onClick={() => click({ gameNumber, gameDate, bonus, drawNumbers })}
+    >
+      <ContentContainer>
+        <Text>
+          <Title>Game</Title>
+          {gameNumber}
+        </Text>
+        <Text>
+          <Title> Date</Title>
+          {`${gameDate.d}/${gameDate.m}/${gameDate.year}`}
+        </Text>
+      </ContentContainer>
+      <NumberContainer>
+        {drawNumbers.map((number, index) => (
+          <Number>{number}</Number>
+        ))}
+      </NumberContainer>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  background-color: #02a8f4;
+  padding: 1rem;
+  border-radius: 5px;
+  min-width: 300px;
+  cursor: pointer;
+  :hover {
+    background-color: #375bcd;
+    outline: 10px;
+  }
+`;
+const ContentContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 4rem;
+  border-bottom: "1px solid #f8f8f8";
+`;
+const Text = styled.p`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 0.8rem;
+  font-weight: bold;
+`;
+
+const Title = styled.span`
+  color: #2b2b2b;
+`;
+
+const NumberContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 0.5rem 1rem;
+`;
+
+const Number = styled.div`
+  background-color: #f8f8fa;
+  width: fit-content;
+  border-radius: 50%;
+  padding: 0.5rem;
+  box-shadow: 0 0 4px #2b2b2b;
+  color: #2b2b2b;
+`;
+
+export default KinoSingleComp;
